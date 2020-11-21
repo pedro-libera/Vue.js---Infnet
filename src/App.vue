a0cdb7192b3038f2b447b7dc26e10ba2d595e4f5
Pedro Gomes <pedro.libera98@gmail.com>
2020-11-21 16:14:41 -0300

    TP1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Bem vindo"/>
+    <div v-for="tarefa in tarefas" :key="tarefa.id">
+      <div v-if="tarefa.estacompleta"
+        class="completa"
+      >
+        <h1>Tarefa numero {{tarefa.id}}</h1>
+        <p>{{ tarefa.descricao }}</p>
+      </div>
+
+      <div v-else
+        class="incompleta"
+      >
+        <h1>Tarefa numero {{tarefa.id}}</h1>
+        <p>{{ tarefa.descricao }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,6 +27,19 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data(){
+    return {
+      tarefas: [
+        {id: 1, descricao: "Tarefa 1", estacompleta: false},
+        {id: 2, descricao: "Tarefa 2", estacompleta: true},
+        {id: 3, descricao: "Tarefa 3", estacompleta: true},
+        {id: 4, descricao: "Tarefa 4", estacompleta: false},
+        {id: 5, descricao: "Tarefa 5", estacompleta: true},
+        {id: 6, descricao: "Tarefa 6", estacompleta: false},
+        {id: 7, descricao: "Tarefa 7", estacompleta: false}
+      ]
+    }
   }
 }
 </script>
@@ -24,5 +52,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.completa {
+  background-color: green;
+}
+
+.incompleta {
+  background-color: red;
 }
 </style>
